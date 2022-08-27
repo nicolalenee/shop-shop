1819c4f3502ac5e78dfebf3b78bd6789b0ff39b4
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-08-26 22:13:27 -0400

    complete state management for home detail productlist and category menu components

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
+import { useStoreContext } from '../../utils/GlobalState';
 
-function CategoryMenu({ setCategory }) {
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+function CategoryMenu() {
+  const [state, dispatch] = useStoreContext();
+  const {categories} = state;
+  const { data: categoryData } = useQuery (QUERY_CATEGORIES);
+  // when component loads and the response from the useQuery() hook returns, the useEffect() hook notices that categoryData is not undefined anymore and runs the dispatch() function, setting our catgory data to the global state!
+  useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to 
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  }, [categoryData, dispatch]);
+
+  // upd8 click handler to upd8 our global state
+  const handleClick = id => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
+    });
+  };
 
   return (
     <div>
@@ -13,7 +35,7 @@ function CategoryMenu({ setCategory }) {
         <button
           key={item._id}
           onClick={() => {
-            setCategory(item._id);
+            handleClick(item._id);
           }}
         >
           {item.name}
--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
 
+
 function ProductItem(item) {
   const {
     image,
