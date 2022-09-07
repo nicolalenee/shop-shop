5904bf73943dd25c0731157e73375a9198452f91
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-09-07 00:17:14 -0400

    fix product list for production

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,49 +1,47 @@
 import React, { useEffect } from 'react';
+import ProductItem from '../ProductItem';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
-
 import { useQuery } from '@apollo/client';
-import { idbPromise } from '../../utils/helpers';
-
-import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
 
-const { currentCategory } = state;
+  const { currentCategory } = state;
 
-const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-useEffect(() => {
-  if (data) {
-    dispatch({
-      type: UPDATE_PRODUCTS,
-      products: data.products,
-    });
-    data.products.forEach((product) => {
-      idbPromise('products', 'put', product);
-    });
-  } else if (!loading) {
-    idbPromise('products', 'get').then((products) => {
+  useEffect(() => {
+    if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: products,
+        products: data.products,
       });
-    });
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) {
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
+      });
+    }
+  }, [data, loading, dispatch]);
+
+  function filterProducts() {
+    if (!currentCategory) {
+      return state.products;
+    }
+
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
-}, [data, loading, dispatch]);
-
-
-function filterProducts() {
-  if (!currentCategory) {
-    return state.products;
-  }
-
-  return state.products.filter(product => product.category._id === currentCategory);
-}
-
 
   return (
     <div className="my-2">
