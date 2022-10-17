c25cf41fcef30af97cfe54d3e09212691960c98b
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-10-17 13:39:28 -0400

    add node legacy build

---
+++ client/build/precache-manifest.0d7a7018325af44c75cc3fb427a07c3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab2ee817cb041a110b2fbd3ded753ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c0ecc951138a15a52d9",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "3bca69f796b5125fc329",
+    "url": "/static/js/2.9f01fe2b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9f01fe2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c0ecc951138a15a52d9",
+    "url": "/static/js/main.38689080.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.38689080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
